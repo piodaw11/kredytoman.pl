09c0ae4b79d49e56bacaada5092c8bd89b230467
Piotrek <>
2022-08-18 15:10:33 +0200

    bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -10,15 +10,15 @@ function MyApp({ Component, pageProps }) {
     <>
       <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
-      <Script strategy="lazyOnload">
+      <Script strategy="lazyOnload" id='google'>
         {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
       </Script>
       <Component {...pageProps} />
     </>
